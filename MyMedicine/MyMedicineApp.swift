630034ca5838129febd71837db93606aed782fe2
mateustibaes <mateus.tibaes@gmail.com>
2025-09-25 22:55:22 -0300

    Initial Commit

---
+++ MyMedicine/MyMedicineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMedicineApp.swift
+//  MyMedicine
+//
+//  Created by Mateus Tibães   on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MyMedicineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMedicineTests/MyMedicineTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMedicineTests.swift
+//  MyMedicineTests
+//
+//  Created by Mateus Tibães   on 25/09/25.
+//
+
+import Testing
+@testable import MyMedicine
+
+struct MyMedicineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
